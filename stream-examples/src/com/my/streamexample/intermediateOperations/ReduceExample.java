dcde10ea1a8c12dd39df02de60ba3a6eb35cf97b
LaluprasadXworkz <laluprasadm.xworkz@gmail.com>
2026-04-29 17:59:27 +0530

    Intermediate Operations for Reduce,Flatmap

--- stream-examples/src/com/my/streamexample/intermediateOperations/ReduceExample.java
+++ stream-examples/src/com/my/streamexample/intermediateOperations/ReduceExample.java
@@ -7,8 +7,11 @@ public class ReduceExample {
 
     public static void main(String[] args) {
 
-        List<Integer> list= Arrays.asList(1, 2, 3, 4, 5);
-        list.stream()
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        int sum = list.stream()
                 .reduce(0, (a, b) -> a + b);
+
+        System.out.println("Sum: " + sum);
     }
 }
